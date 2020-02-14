7f15942e534f465cefa1c04578bd1e96867236f5
Andrew Smith <848smith@gmail.com>
2020-02-14 05:03:08 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,25 @@
-# Add your code here
+class Dog
+  @@all = []
+  
+  def initialize
+    save
+  end
+  
+  def save
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.print_all
+    @@all.each do |dog|
+      "#{dog}"
+    end
+  end
+  
+  def self.clear_all
+    
+  end
+end
